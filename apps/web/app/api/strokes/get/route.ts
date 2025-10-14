f3aaa2778f99f9e7808e7b24279008a9fd739ae4
Avichal Pandey <avichal2018lko@gmail.com>
2025-10-14 14:36:59 +0530

    Fix import path for prismaClient in route.ts

--- apps/web/app/api/strokes/get/route.ts
+++ apps/web/app/api/strokes/get/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prismaClient } from "@repo/db/client";
+import { prismaClient } from "@repo/db";
 
 export async function GET(req: NextRequest)  {
   const { searchParams } = new URL(req.url);
